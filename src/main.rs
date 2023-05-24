8ad82bc0beccc14721ba25e1125f999d58263ea0
websterandy168 <websterandy168@gmail.com>
2023-05-24 11:48:35 -0400

    working but zero-indexed and panics on invalid n

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Enter a positive integer:");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Invalid input");
+
+
+    let n: u32 = input.trim().parse().expect("Please type a positive integer!");
+
+    let mut m = 0;
+    let mut num_n = 1;
+    let mut num_n_minu_1 = 0;
+    let mut num_n_plu_1 = 1;
+    while m < n {
+        num_n_plu_1 = num_n + num_n_minu_1; //makes fib output
+        num_n_minu_1 = num_n; //moves to next n-1 in seq
+        num_n = num_n_plu_1; // moves to next n in seq
+        m = m +1; //track position in fib seq
+    }
+    println!("The {}th number in the fib sequence is: {}", n, num_n_plu_1); 
+
 }
